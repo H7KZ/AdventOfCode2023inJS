31bb5f210b1cefa9a926434b0bc398d4d973750f
H7KZ <74021016+H7KZ@users.noreply.github.com>
2023-12-05 18:46:36 +0100

    day 5

--- 5/index.js
+++ 5/index.js
@@ -4,6 +4,8 @@ const input = readFileSync("./input.txt", "utf8").split("\r\n");
 
 let maps = {};
 
+const startTime = Date.now();
+
 for (let i = 2; i < input.length; i++) {
     if (input[i].match(" map:")) {
         let map = input[i].split(" map:")[0];
@@ -22,57 +24,72 @@ for (let i = 2; i < input.length; i++) {
 
 const mapsValues = Object.values(maps);
 
-const finalSeeds = [];
-
-let seeds = [];
-let seedsCollection = [];
+let lowestSeed = 0;
 
 for (let i = 0; i < input[0].split(": ")[1].split(" ").length; i += 2) {
     const seed = input[0].split(": ")[1].split(" ").map(Number)[i];
     const range = input[0].split(": ")[1].split(" ").map(Number)[i + 1];
 
-    console.log(i);
-    console.log(seed, range);
+    console.log(i / 20 * 100 + "%");
 
     for (let j = seed; j < seed + range; j++) {
-        for (let i = 0; i < mapsValues.length; i++) {
-            seeds.push(j);
+        let currentSeed = j;
 
-            if (seeds.length === 100000000) {
-                seedsCollection.push(seeds);
-                seeds = [];
+        for (let y = 0; y < mapsValues.length; y++) {
+            let mapped = false;
+
+            for (let x = 0; x < mapsValues[y].length; x++) {
+                if (currentSeed >= mapsValues[y][x][1] && currentSeed <= mapsValues[y][x][1] + mapsValues[y][x][2] - 1) {
+                    currentSeed = mapsValues[y][x][0] + currentSeed - mapsValues[y][x][1];
+
+                    mapped = true;
+                }
+
+                if (mapped) break;
             }
-        }
-    }
-
-    seedsCollection.push(seeds);
-    seeds = [];
-
-    for (let i = 0; i < mapsValues.length; i++) {
-        const seedsCollectionCopy = [...seedsCollection];
-
-        for (let j = 0; j < seedsCollectionCopy.length; j++) {
-            for (let y = 0; y < seedsCollectionCopy[j].length; y++) {
-                let mapped = false;
-                for (let x = 0; x < mapsValues[i].length; x++) {
-                    if (seedsCollectionCopy[j][y] >= mapsValues[i][x][1] && seedsCollectionCopy[j][y] <= mapsValues[i][x][1] + mapsValues[i][x][2] - 1) {
-                        seeds[j] = mapsValues[i][x][0] + seedsCollectionCopy[j][y] - mapsValues[i][x][1];
-
-                        mapped = true;
-                    }
-
-                    if (mapped) break;
+                
+            if (y === mapsValues.length - 1) {
+                if (currentSeed < lowestSeed || lowestSeed === 0) {
+                    lowestSeed = currentSeed;
                 }
             }
         }
     }
 
-    console.log(seedsCollection.length);
-    
-    finalSeeds.push(seedsCollection.map(c => c[0]).sort((a, b) => a - b)[0]);
+    // seedsCollection.push(seeds);
+    // seeds = [];
 
-    seedsCollection = [];
+    // for (let i = 0; i < mapsValues.length; i++) {
+    //     const seedsCollectionCopy = [...seedsCollection];
+
+    //     for (let j = 0; j < seedsCollectionCopy.length; j++) {
+    //         for (let y = 0; y < seedsCollectionCopy[j].length; y++) {
+    //             let mapped = false;
+    //             for (let x = 0; x < mapsValues[i].length; x++) {
+    //                 if (seedsCollectionCopy[j][y] >= mapsValues[i][x][1] && seedsCollectionCopy[j][y] <= mapsValues[i][x][1] + mapsValues[i][x][2] - 1) {
+    //                     seeds[j] = mapsValues[i][x][0] + seedsCollectionCopy[j][y] - mapsValues[i][x][1];
+
+    //                     mapped = true;
+    //                 }
+
+    //                 if (mapped) break;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // console.log(seedsCollection.length);
+    
+    // finalSeeds.push(seedsCollection.map(c => c[0]).sort((a, b) => a - b)[0]);
+
+    // seedsCollection = [];
 }
+
+console.log("100%");
+
+console.log(lowestSeed);
+
+console.log(Date.now() - startTime);
 
 // for (let i = 0; i < mapsValues.length; i++) {
 //     const seedsCopy = [...seeds];
@@ -91,4 +108,4 @@ for (let i = 0; i < input[0].split(": ")[1].split(" ").length; i += 2) {
 //     }
 // }
 
-console.log(finalSeeds.sort((a, b) => a - b));
+// console.log(finalSeeds.sort((a, b) => a - b));
